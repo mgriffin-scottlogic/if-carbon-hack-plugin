88fb49f2eeb3acfd93abe50197c8621585356edf
Matthew Griffin <mgriffin@scottlogic.com>
2024-03-28 14:27:52 +0000

    Additional tests and remove unnecessary definition

--- src/__tests__/unit/lib/storage-energy.test.ts
+++ src/__tests__/unit/lib/storage-energy.test.ts
@@ -1,4 +1,4 @@
-import {SECONDS_IN_YEAR} from '../../../lib/constants';
+import {SECONDS_IN_HOUR, SECONDS_IN_YEAR} from '../../../lib/constants';
 import {StorageEnergy} from '../../../lib/storage-energy';
 
 describe('lib/storage-energy: ', () => {
@@ -13,6 +13,38 @@ describe('lib/storage-energy: ', () => {
     });
 
     describe('execute(): ', () => {
+      it('calculates kWh of storing 1Tb for one hour using CCF HDD figures.', async () => {
+        const pluginInstance = StorageEnergy();
+        const inputs = [
+          {
+            duration: SECONDS_IN_HOUR,
+            'storage/drive-size': 10000,
+            'storage/drive-power': 6.5,
+            'storage/data-stored': 1000,
+          },
+        ];
+
+        const response = await pluginInstance.execute(inputs, {});
+        expect(response[0]).toHaveProperty('storage/energy');
+        expect(response[0]['storage/energy']).toBeCloseTo(0.00065);
+      });
+
+      it('calculates kWh of storing 1Tb for one hour using CCF SSD figures.', async () => {
+        const pluginInstance = StorageEnergy();
+        const inputs = [
+          {
+            duration: SECONDS_IN_HOUR,
+            'storage/drive-size': 5000,
+            'storage/drive-power': 6,
+            'storage/data-stored': 1000,
+          },
+        ];
+
+        const response = await pluginInstance.execute(inputs, {});
+        expect(response[0]).toHaveProperty('storage/energy');
+        expect(response[0]['storage/energy']).toBeCloseTo(0.0012);
+      });
+
       it('calculates kWh of storing 1Gb for one year.', async () => {
         const pluginInstance = StorageEnergy();
         const inputs = [
